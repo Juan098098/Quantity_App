6fb9bb2e3191fd7a0b8e68c259af17994151a726
Juan098098 <contrerasdiaz48@gmail.com>
2022-08-25 21:26:39 -0500

    Home Fragmen added

--- Quanty_App/app/src/main/java/com/example/quanty/splash/view/SplashScreenActivity.kt
+++ Quanty_App/app/src/main/java/com/example/quanty/splash/view/SplashScreenActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.quanty.R
 
+
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
